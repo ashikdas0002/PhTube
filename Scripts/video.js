811f7c8d3c53e2dfe069b23203a84833f1f40517
ashikdas0002 <ashikdas0002@gmail.com>
2025-05-07 21:55:12 +0600

    implement category btn with conditional

--- Scripts/video.js
+++ Scripts/video.js
@@ -1,3 +1,13 @@
+// Get Time String 
+const getTimeString = (time) => {
+    const hour = parseInt(time / 3600);
+    let remainingSeconds = time % 3600;
+    const minnutes = parseInt(remainingSeconds / 60);
+    const second = remainingSeconds % 60;
+    return `${hour} Hours ${minnutes} Minutes ${second} Seconds`;
+
+}
+
 // Load Categories 
 const loadCategories = async () => {
     try {
@@ -13,19 +23,36 @@ const loadCategories = async () => {
 
 }
 
+const loadCategoryVideo = async (id) => {
+    // alert(id);
+    const res = await fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`);
+    const data = await res.json();
+    return displayVideos(data.category);
+}
+
+
 
 
 // Show Categories button 
 const displayCategories = (categories) => {
     const categoryContainer = document.getElementById('category-container');
     categories.forEach((item) => {
-        const button = document.createElement('button');
-        button.classList = "bg-slate-400 text-red-950 py-3 px-3 rounded font-bold      ";
-        button.innerText = item.category;
-        categoryContainer.appendChild(button);
-    })
+        const buttonContainer = document.createElement('div');
+        buttonContainer.innerHTML = `
+    <button onclick= "loadCategoryVideo(${item.category_id})" class="btn btn-outline">${item.category}</button>
 
-}
+
+
+`;
+
+        categoryContainer.appendChild(buttonContainer);
+    });
+
+};
+
+// Load Categories Videos
+
+
 
 
 // Load Videos 
@@ -50,31 +77,55 @@ const loadVideos = async () => {
 
 const displayVideos = (videos) => {
     const videoContainer = document.getElementById('videos-container');
+    videoContainer.innerHTML = ""; // Clear previous videos
+    if (videos.length === 0) {
+        videoContainer.classList.remove('grid');
+        videoContainer.innerHTML = `
+        <div class = "min-h-[300px] w-full flex flex-col items-center justify-center gap-5" > 
+        <img src="img/icon.png"  /> 
+        <h1 class = "md:w-[433px] text-[#171717] text-[32px] font-bold text-center ">
+        
+        Oops!! Sorry, There is no  content here
+        </h1>
+        
+        </div>
+        
+        
+        `;
+        return;
 
+    }
+    else {
+        videoContainer.classList.add('grid');
+    }
     videos.forEach((video) => {
         const { title, thumbnail, authors, others } = video;
         console.log(video);
         const card = document.createElement('div');
         card.innerHTML = `
-
-<div class="card bg-base-100 w-96 ">
+<div class="card bg-base-100 w-96 shadow-sm]">
   <figure class="px-10 pt-10 h-[200px] relative">
     <img
       src=${thumbnail}
-      alt="Video Thumbnail"
-      class=" w-full h-full object-cover" />
-    <span class="absolute  bottom-2 bg-black text-white rounded p-2 font-bold">${others.posted_date}</span>
+      alt="Shoes"
+      class=" w-full h-full object-cover " />
+
+ 
+  ${others.posted_date?.length === 0 ? "" : ` <p class="absolute text-xs  bottom-2 bg-[#17171788] text-white font-semibold p-2 rounded-md"> ${getTimeString(others.posted_date)}</p>`}
   </figure>
-  <div class="px-0 py-2 flex gap-2">
-      <div> <img  class="w-[40px] h-[40px] rounded-full" src=${authors[0].profile_picture} /></div>
-<div class="">
-            <h2 class="text-[#171717] text-2xl font-bold font-[Inter]">${title} </h2>
-            <p  class=" text-[#1717179a] text-[20px] font-normal flex items-center gap-3">${authors[0].profile_name}   ${authors[0].verified === true ? ` <img class="w-[20px]"  src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" />
-            <div>` : ""} </p>  
-            <p  class=" text-[#17171779] text-[20px] font-normal">${others.views} Views </p>
-           
+<div class="flex gap-3">
+<div> <img class="w-12 h-12 rounded-full" src=${authors[0].profile_picture}/> </div>
+<div> 
+<h2 class="text-[#171717] text-2xl font-bold "> ${title}</h2>
+<p class=" text-[#17171777] text-[20px] font-normal  flex items-center gap-2">${authors[0].profile_name} ${authors[0].verified === true ? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png"/>` : ""} </p>
+<p class="text-[#17171777] text-[20px] font-normal">${others.views} Views </p>
 </div>
 
+</div>
+  
+</div>
+
+      
     `;
 
 
